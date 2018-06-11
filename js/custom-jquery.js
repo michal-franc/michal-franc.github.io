b8ea129884631f89d74a4c2811ae3b7dc23c16af
Michal Franc <lam.michal.franc@gmail.com>
2018-06-11 19:55:41 +0100

    special hack for Adam Furmanek

--- js/custom-jquery.js
+++ js/custom-jquery.js
@@ -8,4 +8,10 @@ $(function () {
         caption = $(this).attr('title');
         $(this).after('<figcaption class="caption">' + caption + '</figcaption>');
     });
+
+    $('.footnote').each(function() {
+      var refid = $(this).attr('href').substr(1);
+      var href = $("[id='" + refid + "']").children(0).children(1).attr('href');
+      $(this).attr('href', href);
+    });
 })
