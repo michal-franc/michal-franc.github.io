c4b0a876df9f9750db6fff640aca46c32f07551a
Michal Franc <lam.michal.franc@gmail.com>
2024-07-27 23:27:42 +0200

    probably final version of hire me

--- hire-me.md
+++ hire-me.md
@@ -1,22 +1,28 @@
 ---
 layout: carousel
-title: Hire Me
+title: How can I help you?
 permalink: /hire-me/
 ---
-Hey, Throughout my career, I have witnessed software being build from many different angles.
+<br>
+Hey, Throughout my career, I have witnessed software being build from many different angles.  
  
-I have worked as **Frontend, Backend, Devops, Sre, Platform Engineer**.  
-I have held various leadership roles: **Tech Manager, Tech Lead, Architect, Staff/Principal Engineer**.  
-I have build products in early stage where **market fit** was the main concern aswell as optimizsed well established organisations and delt with **problems at scale**, both on the technical and organisation layer.  
-I have worked on problems dealing with **revenue streams** aswells as **reliability, scalability and security**.
+## I have worked as:
+- Frontend 
+- Backend 
+- Devops, Sre, Platform Engineer
 
-This diverse experience gives me a unique ability to understand, in details, how business and technology domains interact.
+## I have held various leadership roles: 
+- Tech Manager
+- Tech Lead
+- Architect 
+- Staff/Principal Engineer
 
 ## I have build products and delt with problems at scale
-- Led a crowdfunding product that has raised hundreds of millions of pounds for charities.
+- Bootstraped a crowdfunding product looking a market fit that ulimately has raised hundreds of millions of pounds for charities.
 - Developed a data analytics product for restaurants, winning the UX UK Award in the "Best Effect on Business Goals" category.
 - Drove platform engineering initiatives to enhance the developer experience.
 - Built and enhanced processes at various levels: team, department, and organization-wide.
+- Optimizsed established organisations and teams deling with **problems at scale**, both on the technical and organisation layer. 
 
 ## I have worked on security / reliability / profitability
 - Saved hundreds of thousands of dollars for customers on their cloud bills.
@@ -27,11 +33,11 @@ This diverse experience gives me a unique ability to understand, in details, how
 - Oversaw the observability domain, managing tens of millions of unique metrics.
 
 ## Most Importantly
-I know how to lead people and use technology to generate business value. For the last 10 years, I have served in leadership oriented roles and positions. I understand how to empower people and how to build collaborative environment where people are motivated to do their best work. My experience and knowledge can enhance the work environment for engineers, making them happier and more productive.
+I know how to lead people and use technology to generate business value. For the last 10 years, I have been leading teams of various sizes. I understand how to empower people and how to build collaborative environment where people are motivated to do their best work. My experience and knowledge can enhance the work environment for engineers, making them happier and more productive.
 
-My main passion is making an impact with technology.
+### This diverse experience gives me a unique ability to understand, in details, how business and technology domains interact.
 
-# If you think I can help you, let's get in touch:
+###  If you think I can help you, let's get in touch:
 - [Linkedin](https://www.linkedin.com/in/michalfranc1/)
 - [Email](mailto:michal.franc@live.com)
 - [Calendly](https://calendly.com/michal-franc)
--- js/carousel.js
+++ js/carousel.js
@@ -15,18 +15,18 @@ let people = [
 		profession: "Head of Engineering at FORM3",
 		description: "Michał is great at juggling multiple responsibilities and is always ready to tackle the most tricky problems. Thanks to his vast technical expertise and decisiveness, he has an outstanding ability to efficiently analyse and diagnose technical issues and take necessary actions to solve the problem at hand, even in crisis situations. As a Staff Engineer, Michał continuously provided technical designs and recommendations. Advice that he gave was always well thought-through, based on a deep and multi-faceted analysis of the problem. Moreover, he always incorporated business perspective in his proposals, which I greatly appreciated."
 	},
-	{
-		photo: 'url("https://media.licdn.com/dms/image/D4E03AQHuw5JB4sUowQ/profile-displayphoto-shrink_100_100/0/1697195915170?e=1727308800&v=beta&t=QWpXNHf5BfMN1zXFaDclyeVFw7QTPB5xJZIjw5uNU5Q")',
-		name: "Jake Scutt",
-		profession: "Senior Product Manager at FORM3",
-		description: "Michal is business focused and a very experienced engineer, he is happy to wear many hats to ensure he and the teams he works with are delivering the most value. He is a fountain of knowledge and strategic ideas, always delivering quality work and happy to receive feedback. This approach and his openness to collaboration makes him the type of engineer any product person would love to work with. Michal as a team lead and as an individual contributor cares deeply about the people and projects he works on. He is committed to making an impact and successfully motivates and influences people to make this happen."
-	},
-	{
-		photo: "url('https://media.licdn.com/dms/image/C4E03AQGp3F59xz3uCg/profile-displayphoto-shrink_100_100/0/1619701737272?e=1727308800&v=beta&t=AFMKGeji_QMVC2S6qG0yzrg7LFh-LGrLOeZc0zioCp8')",
-		name: "Patryk Żmigrodzki",
-		profession: "Senior Software Engineer at FORM3",
-		description: "I worked with Michal in various capacities, including him as a Senior Software Engineer, Team Leader, and Staff Engineer, and I can confidently say that he is one of the best engineers I have ever encountered. As a Staff Engineer, Michal truly could see the bigger picture. He effectively planned long-term initiatives and facilitated efforts across an engineering organization composed of hundreds of engineers. His strategic vision and coordination skills ensure that projects are not only successful but also aligned with the overarching goals of the company."
-	}
+//	{
+//		photo: 'url("https://media.licdn.com/dms/image/D4E03AQHuw5JB4sUowQ/profile-displayphoto-shrink_100_100/0/1697195915170?e=1727308800&v=beta&t=QWpXNHf5BfMN1zXFaDclyeVFw7QTPB5xJZIjw5uNU5Q")',
+//		name: "Jake Scutt",
+//		profession: "Senior Product Manager at FORM3",
+//		description: "Michal is business focused and a very experienced engineer, he is happy to wear many hats to ensure he and the teams he works with are delivering the most value. He is a fountain of knowledge and strategic ideas, always delivering quality work and happy to receive feedback. This approach and his openness to collaboration makes him the type of engineer any product person would love to work with. Michal as a team lead and as an individual contributor cares deeply about the people and projects he works on. He is committed to making an impact and successfully motivates and influences people to make this happen."
+//	},
+//	{
+//		photo: "url('https://media.licdn.com/dms/image/C4E03AQGp3F59xz3uCg/profile-displayphoto-shrink_100_100/0/1619701737272?e=1727308800&v=beta&t=AFMKGeji_QMVC2S6qG0yzrg7LFh-LGrLOeZc0zioCp8')",
+//		name: "Patryk Żmigrodzki",
+//		profession: "Senior Software Engineer at FORM3",
+//		description: "I worked with Michal in various capacities, including him as a Senior Software Engineer, Team Leader, and Staff Engineer, and I can confidently say that he is one of the best engineers I have ever encountered. As a Staff Engineer, Michal truly could see the bigger picture. He effectively planned long-term initiatives and facilitated efforts across an engineering organization composed of hundreds of engineers. His strategic vision and coordination skills ensure that projects are not only successful but also aligned with the overarching goals of the company."
+//	}
 ];
 
 imgDiv.style.backgroundImage = people[0].photo;
